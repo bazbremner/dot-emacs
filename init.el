491364e9fb07ca23f51187102432799695165d7c
Barrie Bremner <baz@barriebremner.com>
2017-11-07 16:43:26 +0100

    Set background face to play nicely with iTerm and solarized themes

--- init.el
+++ init.el
@@ -163,6 +163,12 @@
 
 (custom-set-variables
  '(frame-background-mode (quote dark)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:background "nil")))))
 
 
 (custom-set-faces
